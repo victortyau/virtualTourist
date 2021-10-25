183b2f92639a93d1c06978b3f17e8f5cd7e6c6f9
Victor Tejada Yau <victortyau@gmail.com>
2021-10-25 00:12:48 -0500

    Initial Commit

---
+++ virtualTourist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  virtualTourist
+//
+//  Created by Victor Tejada Yau on 10/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

69935fb1a06eae4c3e41801df12ea1405134d587
Victor Tejada Yau <victortyau@gmail.com>
2021-10-30 13:40:20 -0500

    working on map and gesture recognition

--- virtualTourist/Controller/MapViewController.swift
+++ virtualTourist/Controller/MapViewController.swift
@@ -8,14 +8,23 @@
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController, MKMapViewDelegate {
+class MapViewController: UIViewController {
 
+    var EditPin: Bool = false
+    var gestureStart: Bool = false
+    var storedPins: [Pin] = []
+    
     @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        mapView.delegate = self
+        
     }
+    
+    
+}
+
+extension MapViewController:  MKMapViewDelegate {
     
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         let reuseId = "pin"
@@ -42,5 +51,33 @@ class MapViewController: UIViewController, MKMapViewDelegate {
     
     func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
         print((view.annotation?.subtitle ?? "") ?? "")
+    }
+}
+
+extension MapViewController: UIGestureRecognizerDelegate {
+     
+    @IBAction func responseLongTapAction(_ sender: Any){
+        print("padentro")
+        print(gestureStart)
+        if gestureStart {
+            print("padentro 2")
+            let gestureRecognizer = sender as! UILongPressGestureRecognizer
+            let gestureTouchLocation = gestureRecognizer.location(in: mapView)
+            addPinToMap(point: gestureTouchLocation)
+            gestureStart = false
+        }
+    }
+    
+    func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        print("Tamos here")
+        gestureStart = true
+        return true
+    }
+    
+    func addPinToMap(point: CGPoint) {
+        let coordinate = mapView.convert(point, toCoordinateFrom: mapView)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        mapView.addAnnotation(annotation)
     }
 }

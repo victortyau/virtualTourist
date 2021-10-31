d1c5c6f216a7e218e4adbdcb35098d4a7de77bc1
Victor Tejada Yau <victortyau@gmail.com>
2021-10-31 00:37:56 -0500

    setting network services

---
+++ virtualTourist/Network/NetworkHelper.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkHelper.swift
+//  virtualTourist
+//
+//  Created by Victor Tejada Yau on 10/31/21.
+//
+
+import Foundation
---
+++ virtualTourist/Network/ServiceClient.swift
@@ -0,0 +1,17 @@
+//
+//  ServiceClient.swift
+//  virtualTourist
+//
+//  Created by Victor Tejada Yau on 10/31/21.
+//
+
+import Foundation
+
+class ServiceClient {
+    
+    enum Endpoints {
+        static let baseUrl = "https://api.flickr.com/services/rest/"
+        static let apiKey = "e2bdf455f99ac7216300a9cdaad57479"
+        static let secretKey = "1296074b77ac4aba"
+    }
+}
